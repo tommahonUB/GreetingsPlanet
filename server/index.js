d2bd0692a468006bdb859d46d6e305afb5136823
Tom Mahoney <tommahon@buffalo.edu>
2021-02-28 23:31:05 -0500

    patched some bugs, added new files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const { response } = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 //Navigation
 
@@ -12,45 +14,66 @@ const viewsPath = path.join(clientPath,'/views/')
 //basic server
 
 const app = express();
-
 app.use(express.static(staticPath));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+	name: 'Tom',
+	secret: 'eachcathad7kittens',
+	saveUninitialized: true,
+	resave: true,
+	cookie: {
+		maxage: 1000*60*60*24*3,
+	}
+}));
+
 app.listen(2000);
 
 //setting views
+app.set('view engine','ejs');
+app.set('views',viewsPath);
 
-app.set('view engine', 'ejs');
-app.set('views', viewsPath);
 
-//visitor counter
 
-var x = 0;
-
-const counter = function(req, res, next) {
-	x++;
-	console.log(x);
-	next();
-
-}
-
-app.use(counter);
-
-var username = '';
+//routes
 
 app.get('/', function(req, res) {
-	res.render('index', {nomen: username});
+	res.render('index', {data: req.session});
 });
 
-app.get('/Ryu', counter, function(req, res) {
-	res.render('Ryu',{counter: x});
+app.get('/Ryu', function(req, res) {
+	res.render('Ryu', {data: req.session});
 });
 
 app.get('/ChunLi', function(req, res) {
-	res.render('ChunLi');
+	res.render('ChunLi', {data: req.session});
+});
+
+app.get('/Guile', function(req, res) {
+	res.render('Guile', {data: req.session});
+});
+
+app.get('/Zangief', function(req, res) {
+	res.render('Zangief', {data: req.session});
+});
+
+app.get('/Dhalsim', function(req, res) {
+	res.render('Dhalsim', {data: req.session});
+});
+
+app.get('/Ken', function(req, res) {
+	res.render('Ken', {data: req.session});
+});
+
+app.get('/Blanka', function(req, res) {
+	res.render('Blanka', {data: req.session});
+});
+
+app.get('/EHonda', function(req, res) {
+	res.render('EHonda', {data: req.session});
 });
 
 app.post('/welcome', (req, res) => {
-	console.log(req.body.vistorname);
-	username=req.body.visitorname;
-	res.redirect('/');
+	console.log(req.body);
+	req.session.username=req.body.nombre;
+	res.send('SUCCESS');
 });
